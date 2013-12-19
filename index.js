cd1e484b98c2efb1ce35ce39ccc57b1614beec50
mz121star <mz121star@gmail.com>
2013-12-19 20:49:44 +0800

    miaoz

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var framework = require('partial.js');
 var http = require('http');
 
-var debug = true;
-var port = framework.config['default-port'];
-framework.run(http, debug, port);
-framework.log('http://{0}:{1}/'.format(framework.ip, framework.port));
+var debug = false;
+/*var port = framework.config['default-port'];*/
+framework.run(http, debug, 8000);
+
 console.log('http://{0}:{1}/'.format(framework.ip, framework.port));
