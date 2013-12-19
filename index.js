1e8927344c8d9809bbccbd26e21253adbd21c15d
mz121star <mz121star@gmail.com>
2013-12-19 21:12:25 +0800

    miaoz

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 
 var debug = false;
 /*var port = framework.config['default-port'];*/
-framework.run(http, debug, 8000);
+framework.run(http, 8000);
 
 console.log('http://{0}:{1}/'.format(framework.ip, framework.port));
